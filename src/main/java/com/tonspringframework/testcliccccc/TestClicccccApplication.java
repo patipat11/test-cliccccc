73f5c8907ac2f43ccfa7a1e393e0c5bfa3402eed
Patipat Sahasapanan <patipat.saha@gmail.com>
2018-08-23 18:00:16 +0700

    update

---
+++ src/main/java/com/tonspringframework/testcliccccc/TestClicccccApplication.java
@@ -0,0 +1,12 @@
+package com.tonspringframework.testcliccccc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestClicccccApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestClicccccApplication.class, args);
+    }
+}
